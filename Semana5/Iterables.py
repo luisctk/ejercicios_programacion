3c7c3747382aa2d1f362bbab2c0d51acb3d344ed
Luis <luisctk02@gmail.com>
2026-08-19 02:01:28 -0400

    Fixing error

--- Semana5/Iterables.py
+++
@@ -1,6 +0,0 @@
-firt_list = ['aprendiendo','cada','con']
-second_list = ['python','dia','lifter']
-list = [firt_list,second_list]
-
-for index in range(0,len(firt_list)):
-    print(f' { firt_list[index]} {second_list[index]}' )
--- Semana6/two_functions.py
+++
@@ -1,8 +0,0 @@
-def firts():
-    print('Soy la primera funcion')
-
-def second():
-    firts()
-    print('estoy llamando a la segunda')
-
-second()
